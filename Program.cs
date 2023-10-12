1e33a368e0d544173f4be6b00310571f4023f8b7
Hugge25 <rudenfors.eric@gmail.com>
2023-10-12 11:03:58 +0200

    yes

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿const string input = "SNAKE";
+Stack<char> stack = new Stack<char>();
+
+foreach(char c in input){
+    stack.Push(c);
+
+}
+
+while(stack.Count != 0){
+    Console.Write(stack.Pop());
+}
